cdc3970b890c02b87f059f60283fab65a252cf2f
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2025-11-10 16:43:55 -0300

    refactor: remove app service

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -19,5 +19,6 @@ import Env from '~/shared/env';
   ],
   controllers: [AuthController],
   providers: [LocalStrategy, JwtStrategy, ...authUseCasesProviders],
+  exports: [ ...authUseCasesProviders],
 })
 export class AuthModule {}
