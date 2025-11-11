e93c18c4ed61fa09afefe7dc4a205348a529924e
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2025-11-11 10:53:53 -0300

    feat: cant delete remaining admin

--- apps/api/src/modules/users/use-cases/delete-user/index.ts
+++ apps/api/src/modules/users/use-cases/delete-user/index.ts
@@ -1,4 +1,8 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { DeleteUserParamsDTO, UserDTO } from '@repo/api';
 import UserRepository from '~/services/database/typeorm/repositories/user.repository';
 import { UseCase } from '~/shared/core/use-case';
@@ -22,6 +26,17 @@ export default class DeleteUser
         'You can only edit your own account information',
       );
     }
+
+    if (isSameUser && currentUser.isAdmin) {
+      const remainingAdminsCount = await this.userRepository.countAdmins();
+      const isOnlyAdmin = remainingAdminsCount === 1;
+      if (isOnlyAdmin) {
+        throw new BadRequestException(
+          'Unable to delete the last remaining admin user.',
+        );
+      }
+    }
+
     return this.userRepository.delete(id);
   }
 }
