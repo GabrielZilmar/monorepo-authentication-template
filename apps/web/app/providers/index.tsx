8778aec9f7fcb29e3b835caa6f497be3afd918ef
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2026-01-19 12:08:34 -0300

    chore: add tanstack react-query

--- apps/web/app/providers/index.tsx
+++ apps/web/app/providers/index.tsx
@@ -1,16 +1,24 @@
 "use client";
 
-import { HeroUIProvider } from "@repo/ui";
+import { HeroUIProvider, ToastProvider } from "@repo/ui";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const RootProvider = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const [queryClient] = useState(() => new QueryClient());
   const router = useRouter();
 
-  return <HeroUIProvider navigate={router.push}>{children}</HeroUIProvider>;
+  return (
+    <HeroUIProvider navigate={router.push}>
+      <ToastProvider />
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </HeroUIProvider>
+  );
 };
 
 export default RootProvider;
--- packages/ui/src/lib/toast.ts
+++ packages/ui/src/lib/toast.ts
@@ -8,18 +8,34 @@ type ToastColor =
   | 'warning'
   | 'danger';
 
-const useToast = () => {
-  const createToast = (color: ToastColor) => (params: ToastProps) =>
+export const useToastActions = () => {
+  const createToast = (color: ToastColor, params: ToastProps) =>
     addToast({ ...params, color });
 
   return {
-    addDefault: createToast('default'),
-    addPrimary: createToast('primary'),
-    addSecondary: createToast('secondary'),
-    addSuccess: createToast('success'),
-    addWarning: createToast('warning'),
-    addError: createToast('danger'),
+    addDefaultToast: (params: ToastProps) =>
+      createToast('default', { ...params }),
+    addPrimaryToast: (params: ToastProps) =>
+      createToast('primary', { ...params }),
+    addSecondaryToast: (params: ToastProps) =>
+      createToast('secondary', { ...params }),
+    addSuccessToast: (params: ToastProps) =>
+      createToast('success', { ...params }),
+    addWarningToast: (params: ToastProps) =>
+      createToast('warning', { ...params }),
+    addErrorToast: (params: ToastProps) => createToast('danger', { ...params }),
+
+    showDefaultToast: (title: string) =>
+      createToast('default', { title } as ToastProps),
+    showPrimaryToast: (title: string) =>
+      createToast('primary', { title } as ToastProps),
+    showSecondaryToast: (title: string) =>
+      createToast('secondary', { title } as ToastProps),
+    showSuccessToast: (title: string) =>
+      createToast('success', { title } as ToastProps),
+    showWarningToast: (title: string) =>
+      createToast('warning', { title } as ToastProps),
+    showErrorToast: (title: string) =>
+      createToast('danger', { title } as ToastProps),
   };
 };
-
-export default useToast;
